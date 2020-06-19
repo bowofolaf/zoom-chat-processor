6d0bf7d55bb574a91004f5c779ebc5c49b3e5d8e
Bowofola 👨🏾‍💻 <bowofola@gmail.com>
2020-06-19 15:47:35 -0500

    add output-content-type flag, types to flags, and class for processor options. add exporter factory logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
-import { processChat } from './parse/zoom-chat-parser.js'
-import JsonExporter from './export/json-exporter.js'
+import { processChat } from './parse/zoom-chat-parser.js';
+import JsonExporter from './export/json-exporter.js';
+import CsvExporter from './export/csv-exporter.js';
 
-export function run(inputFile, group, outputFile) {
-    processChat(inputFile).then(chats => {
-        let exporter = new JsonExporter(group, outputFile);
-        exporter.write(chats);
-    });
-    
+export const OUTPUT_CONTENT_TYPES = ['csv', 'json'];
+
+export function run(chatProcessorOptions) {
+    if (OUTPUT_CONTENT_TYPES.indexOf(chatProcessorOptions.outputContentType.toLowerCase()) == -1) {
+        throw new Error(`Output content type: [${chatProcessorOptions.outputContentType}] not supported. Please provide
+        one of the following: ${OUTPUT_CONTENT_TYPES}`)
+    }
+
+    processChat(chatProcessorOptions.input)
+        .then(chats => {
+            let exporter = getExporter(chatProcessorOptions);
+            exporter.write(chats);
+        })
+        .catch(console.error)
+}
+
+function getExporter(chatProcessorOptions) {
+    switch (chatProcessorOptions.outputContentType) {
+        case 'csv':
+            return new CsvExporter(chatProcessorOptions.group, chatProcessorOptions.outputFile, chatProcessorOptions.delimiter);
+            break;
+        case 'json':
+            return new JsonExporter(chatProcessorOptions.group, chatProcessorOptions.outputFile)
+            break;
+        default:
+            throw new Error(`Output content type: [${chatProcessorOptions.outputContentType}] not supported. Please provide
+        one of the following: ${OUTPUT_CONTENT_TYPES}`)
+    }
+}
+
+export class ChatProcessorOptions {
+    constructor(input, outputFile, group, outputContentType) {
+        this.input = input;
+        this.outputFile = outputFile;
+        this.group = group;
+        this.outputContentType = outputContentType;
+    }
 }
--- src/cli.js
+++ src/cli.js
@@ -1,30 +1,47 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
-import { run } from './app.js'
+import { run, OUTPUT_CONTENT_TYPES, ChatProcessorOptions } from './app.js'
+
+const DEFAULT_OUTPUT_CONTENT_TYPE = 'csv';
 
 const builder = (yargs) => {
     yargs
-        .positional('input', {
-            describe: 'absolute path to zoom chat file'
+        .option('input', {
+            type: 'string',
+            alias: 'i',
+            describe: 'absolute path to zoom chat file or directory containing files'
         })
-        .positional('output', {
+        .option('output', {
+            type: 'string',
+            alias: 'o',
             describe: 'path to desired output file'
         })
         .option('group', {
+            type: 'boolean',
             describe: 'flag specifying whether to group messages by author',
             default: false
+        })
+        .option('output-content-type', {
+            type: 'string',
+            describe: 'content type for the output file.',
+            default: 'json',
+            choices: OUTPUT_CONTENT_TYPES
         })
 };
 
 yargs
     .usage('Usage $0 <command> [options]')
-    .command(['convert <input> <output> [group]', '$0'],
+    .command(['convert [input] [output] [group] [output-content-type]', '$0'],
         'Parses a zoom chat file and exports in a given format', builder, (argv) => {
-            run(argv.input, argv.group, argv.output)
+            let chatProcessorOptions = new ChatProcessorOptions(
+                argv.input, argv.output, argv.group, argv.outputContentType
+            )
+            run(chatProcessorOptions)
         })
-    .example('convert -f /path/to/chat.txt', 'converts the file /path/to/chat.txt to a csv of author and message')
-    .help('h')
+    .demandOption(['i', 'o'])
+    .example('convert -i /path/to/chat.txt -o /path/to/export', 'converts the file /path/to/chat.txt to a json array of author, timestamp and message')
     .alias('v', 'version')
-    .alias('f', 'file')
-    .demandCommand()
+    .normalize('input')
+    .normalize('output')
+    .help('h')
     .argv
